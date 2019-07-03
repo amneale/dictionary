ad15a1f1c26945cf323020d1967ebcda89c4fa66
Adam Neale <adam@adam-neale.co.uk>
2019-07-03 10:27:55 +0100

    dictionary spec

---
+++ spec/Dictionary/DictionarySpec.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace spec\Lexicon\Dictionary;
+
+use PhpSpec\ObjectBehavior;
+
+class DictionarySpec extends ObjectBehavior
+{
+    private const STRINGS = ['foo', 'bar', 'baz'];
+
+    public function let(): void
+    {
+        $this->beConstructedWith(...self::STRINGS);
+    }
+
+    public function it_is_countable(): void
+    {
+        $this->shouldImplement(\Countable::class);
+        $this->count()->shouldReturn(3);
+    }
+
+    public function it_is_iterable(): void
+    {
+        $this->shouldImplement(\Traversable::class);
+        $this->shouldIterateAs(new \ArrayIterator(self::STRINGS));
+    }
+
+    public function it_returns_an_array_of_values(): void
+    {
+        $this->toArray()->shouldEqual(self::STRINGS);
+    }
+}
